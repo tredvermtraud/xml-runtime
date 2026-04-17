95eeca79895c50780c65280dd3ea3f6517373d4a
Tobias Runkel <runkel@edv-ermtraud.net>
2026-04-17 08:38:53 +0200

    added prefix to root element

--- src/Xml/XmlObjectMapper.php
+++ src/Xml/XmlObjectMapper.php
@@ -47,8 +47,11 @@ final class XmlObjectMapper implements XmlCodecInterface
             ? $rootMetadata->namespaces
             : [];
 
+        $prefix = $this->preferredPrefixForNamespace($namespaceDeclarations, $rootMetadata->namespace);
+        $prefix = $prefix !== null ? $prefix . ':' : '';
+
         $root = $rootMetadata->namespace !== null
-            ? $document->createElementNS($rootMetadata->namespace, $rootMetadata->name)
+            ? $document->createElementNS($rootMetadata->namespace, $prefix.$rootMetadata->name)
             : $document->createElement($rootMetadata->name);
 
         $document->appendChild($root);
@@ -219,7 +222,6 @@ final class XmlObjectMapper implements XmlCodecInterface
     private function readRootMetadata(ReflectionClass $reflection): XmlRoot
     {
         $attributes = $reflection->getAttributes(XmlRoot::class);
-        error_log(print_r($attributes, true));
         if ($attributes === []) {
             return new XmlRoot($reflection->getShortName());
         }
